e9955c6f512f61448798c2fff461a89541f70609
wubian120 <120793495@qq.com>
2015-10-16 15:57:21 +0800

    nearly complete. from office pc

--- QuickSortTesting.cpp
+++ QuickSortTesting.cpp
@@ -4,44 +4,95 @@
 
 #include <iostream>
 #include <vector>
+#include "SrcPoint.h"
+#include <stdlib.h>
+
 using namespace std;
 //this method is about testing the quick sort algorithm
 // testing the parameters number;
 
+
+
 ///from min to max;
-void QuickSort(vector<double>& can)
+void QuickSort(Container& can,int s, int e, int sort_d)
 {
 	if(can.empty()){
 		return;
 	}
 
-	int r = can.size() - 1;
-	int l = 0;
+	if(s>e){return;}
 
-	if(l>r){return;}
-	double pivot = can[l];
+	int r = e;
+	int l = s;
+	SrcPoint* pivot = can.at(s);
 
 	while(l!=r){
 
-		while(pivot <= can[r] && l<r){
-			r--;
+		if(sort_d == 0)
+		{
+			while(pivot->x <= can.at(r)->x && l<r){
+				r--;
+			}
+			while(pivot->x >= can.at(l)->x && l<r){
+				l++;
+			}
 		}
 
-		while(pivot >= can[l] && l<r){
-			l++;
+		if(sort_d == 1)
+		{
+			while(pivot->y <= can.at(r)->y && l<r){
+				r--;
+			}
+			while(pivot->y >= can.at(l)->y && l<r){
+				l++;
+			}
 		}
 
 		if(l<r){
-			double t = can[l];
-			can[l]   = can[r];
-			can[r]   = t;
+			SrcPoint* t = can.at(l);
+			can.at(l)   = can.at(r);
+			can.at(r)   = t;
 		}
 
 	}
-	can[0] = can[l];
-	can[l] = pivot;
+	can.at(s)= can.at(l);
+	can.at(l) = pivot;
 
-//	QuickSort();
+	QuickSort(can,s,l-1, sort_d);
+	QuickSort(can,l+1,e, sort_d);
+}
+
+
+void PrintContainer(const Container& can)
+{
+	if(can.empty())
+	{
+		cout<<"empty"<<endl;
+		return;
+	}
+	int i = 0;
+	Container::const_iterator itr = can.begin();
+	for(;itr!=can.end();++itr){
+
+		cout<<" the ["<<i<<"] is :"<<(*itr)->x<<"\t";
+		cout<<(*itr)->y<<endl;
+		i++;
+	}
+}
+
+void BuildContainerExample(Container& can)
+{
+	SrcPoint* s[12];
+
+	can.clear();
+	for(int i=0;i<12;++i){
+		s[i]  = new SrcPoint;
+		s[i]->x = (double)rand();
+		s[i]->y = (double)rand();
+		can.push_back(s[i]);
+	}
+
+
 
 }
 
@@ -49,10 +100,22 @@ void QuickSort(vector<double>& can)
 int main()
 {
 
+	Container can;
+	cout<<"can size: "<<can.size()<<endl;
+	BuildContainerExample(can);
 
+	cout<<"can size: "<<can.size()<<endl;
+	PrintContainer(can);
 
+	int s = 0;
+	int e = can.size()-1;
+	QuickSort(can,s,e,0);
+	PrintContainer(can);
 
+	QuickSort(can,s,e,1);
+	PrintContainer(can);
 
+//	cout<<"i:"<<i<<endl;
 
 	cout<<"The end!"<<endl;
 	return 0;
